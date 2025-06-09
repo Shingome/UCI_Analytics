2a8ef24ff004b9cca3b154e15962f8052059720e
shingome <mishakkoles@gmail.com>
2025-06-10 02:32:22 +0300

    add shap_interpretation.py

---
+++ src/shap_interpretation.py
@@ -0,0 +1,18 @@
+import shap
+import joblib
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+
+if __name__ == "__main__":
+    model = joblib.load("../models/xgboost_model.pkl")
+    df = pd.read_csv("../data/processed/train_balanced.csv")
+    X = df.drop("income", axis=1)
+
+    explainer = shap.Explainer(model)
+    shap_values = explainer(X)
+
+    shap.summary_plot(shap_values, X, show=False)
+    plt.savefig("../outputs/shap_summary_plot.png")
+    plt.close()
